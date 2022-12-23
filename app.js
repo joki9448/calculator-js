d0ba489f1bf27e722fbdb9269ad02042aedfe9d8
John Kim <joki9448@gmail.com>
2022-12-23 18:07:41 -0500

    started calculation functionality

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const buttons = document.querySelectorAll('button');
 const screenDisplay = document.querySelector('.screen');
 
+let calculation = [];
+let accumulativeCalculation;
+
+function calculate(button) {
+    const value = button.textContent
+
+    calculation.push(value)
+    accumulativeCalculation = calculation.join('');
+    screenDisplay.textContent = accumulativeCalculation
+
+    console.log(calculation)
+};
+
+buttons.forEach((btn) => btn.addEventListener('click', () => calculate(btn)))
