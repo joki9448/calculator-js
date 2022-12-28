30d8b9a94f67b1810c6a3ddd3c271ac7f91840b8
John Kim <joki9448@gmail.com>
2022-12-27 19:34:49 -0500

    finished

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ let accumulativeCalculation;
 
 function calculate(btn) {
     const value = btn.textContent
-
-    calculation.push(value)
-    accumulativeCalculation = calculation.join('');
-    screenDisplay.textContent = accumulativeCalculation
-
-    console.log(calculation)
+    if (value === "CLEAR") {
+        calculation = []
+        screenDisplay.textContent = '.'
+    } else if (value === "=") {
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+        calculation.push(value)
+        accumulativeCalculation = calculation.join('');
+        screenDisplay.textContent = accumulativeCalculation
+    }
+    // console.log('calculation', calculation)
 };
 
 buttons.forEach((btn) => btn.addEventListener('click', () => calculate(btn)))
